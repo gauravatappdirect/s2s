6289ff0740e48179ca02d5048bb44cb40c250dbe
Gaurav Kumar <gaurav.kumar@appdirect.com>
2017-03-19 17:00:44 +0530

    spring boot, spring data rest, spring data jpa, angular

---
+++ first/src/main/java/first/PersonRepository.java
@@ -0,0 +1,13 @@
+package first;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+}
